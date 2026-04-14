14ef629b55dbb825e9ac83600275a834fece2482
marimo12356 <hirita_rento@koushi-intec.co.jp>
2026-04-14 15:23:42 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,12 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Car_Chapter15 car1=new Car_Chapter15();
+		car1.changeGear(3);
+		car1.run();
+	}
+
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,31 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	private int gear=1;
+	private int speed=10;
+	
+	
+	
+	
+	public void changeGear(final int afterGear) {
+		if(afterGear==1) {
+			this.speed=10;
+		}else if(afterGear==2) {
+			this.speed=20;
+		}else if(afterGear==3) {
+			this.speed=30;
+		}else if(afterGear==4) {
+			this.speed=40;
+		}else if(afterGear==5) {
+			this.speed=50;
+		}else if(afterGear==6) {
+			this.speed=60;
+		}
+		
+		
+			System.out.println("ギア"+this.gear+"から"+afterGear+"に切り替えました");
+	}
+	public void run() {
+			System.out.println("速度は時速"+this.speed+"です");
+	}
+}
